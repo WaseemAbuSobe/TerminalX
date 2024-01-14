6279bf4a8d3766cf7d1bc34726e407a7200e3ffe
waseem <waseem-abusobe@hotmail.com>
2024-01-14 16:33:59 +0200

    edite the fiels and add confogs file

--- logic/ui/OnSale.ts
+++ logic/ui/OnSale.ts
@@ -1,27 +1,16 @@
 import { Locator, Page } from 'playwright';
-export class OnSale{
-    private page:Page
-    private onSaleBtn:Locator
+import { BasePage } from '../../infra/ui/base-page';
+export class OnSale extends BasePage{
+
     private oldPrice!:Locator
     private newPrice!:Locator
     private off!:Locator
     constructor(page:Page){
-        this.page = page;
+        super(page)
         this.initPage()
-        this.onSaleBtn = this.page.locator('a[href="/on-sale"]')
-    }
-    private async initPage(){
-        await this.page.waitForLoadState()       
-    }
-    private async initLocators(){
         this.oldPrice = this.page.locator('//*[@id="app-root"]/div[2]/main/div[2]/div/div[3]/div[3]/div[2]/ol/li[1]/div[3]/div[1]/div[1]/div/div[2]')
         this.newPrice = this.page.locator('//*[@id="app-root"]/div[2]/main/div[2]/div/div[3]/div[3]/div[2]/ol/li[1]/div[3]/div[1]/div[1]/div/div[1]')
         this.off = this.page.locator('//*[@id="app-root"]/div[2]/main/div[2]/div/div[3]/div[3]/div[2]/ol/li[1]/div[3]/div[1]/div[1]/a')
-    }
-    public async flowToOnSalePage(){
-        await this.onSaleBtn.click()
-        await this.initPage()
-        await this.initLocators()
     }
     public async createSale(){
         const oldPrice:number = parseFloat((await this.oldPrice.innerText()).replace(/[^\d.]/g, ''))
@@ -32,5 +21,4 @@ export class OnSale{
         const newPrice:number = parseFloat((await this.newPrice.innerText()).replace(/[^\d.]/g, ''))
         return newPrice
     }
-
 }
--- tests/onsale.spec.ts
+++ tests/onsale.spec.ts
@@ -1,25 +1,30 @@
 import {Page, expect, test} from '@playwright/test'
 import { BrowserWrapper } from '../infra/ui/brwoser-wrapper'
 import { OnSale } from '../logic/ui/OnSale';
+import { NavBar } from '../logic/ui/NavBar';
+import * as fs from 'fs';
 
 let browser:BrowserWrapper
 let page:Page
+const configFile = fs.readFileSync('./configs/ui-urls.json', 'utf-8');
+const UI_URLS = JSON.parse(configFile);
 test.beforeEach(async()=>{
     browser = new BrowserWrapper()
-    page = await browser.getPage('https://www.terminalx.com/');
+    page = await browser.getPage(UI_URLS.mainPage);
 })
 test.afterEach(async()=>{
     await browser.closeBrowser()
 })
 
 test('Flow To On Sale Page',async()=>{
-    const onSale = new OnSale(page)
-    await onSale.flowToOnSalePage()
+    const navbar = new NavBar(page)
+    await navbar.flowToOnSale()
     await expect(page).toHaveURL('https://www.terminalx.com/on-sale')
 })
 
 test('Validate The Sale',async()=>{
+    const navbar = new NavBar(page)
+    await navbar.flowToOnSale()
     const onSale = new OnSale(page)
-    await onSale.flowToOnSalePage()
     expect(await onSale.createSale()).toEqual(await onSale.getNewPrice())
 })
