d5ab1d5f85cfb63e08003cb771ede7be0c0da45a
waaed azzam <waaed013@gmail.com>
2024-01-14 11:10:24 +0200

    first commit

---
+++ infra/ui/base-page.ts
@@ -0,0 +1,11 @@
+import { Page } from "playwright";
+
+export class BasePage {
+    protected page: Page;
+    constructor(page: Page) {
+        this.page = page;
+    }
+    initPage = async () => {
+        await this.page.waitForLoadState("load")
+    }
+} 
