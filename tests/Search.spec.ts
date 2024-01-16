9255570b638c5828dda3aca3037db154ef5c0e3b
waseem <waseem-abusobe@hotmail.com>
2024-01-16 15:58:29 +0200

    update

--- tests/Search.spec.ts
+++ tests/Search.spec.ts
@@ -4,25 +4,21 @@ import {SearchPage} from '../logic/pages/searchPage';
 import {websiteUrl} from '../config/ui-urls.json';
 import {brandSearch} from '../config/brandSearch.json'
 
-
 test.describe('search test', ()=>{
     let browserWrapper : BrowserWrapper;
 
     test.beforeAll(async()=>{
         browserWrapper = new BrowserWrapper();
-
     })
 
     test.beforeEach(async()=>{
         await browserWrapper.maximizeWindow();
     })
     
-
     test.afterEach(async()=>{
         await browserWrapper.closeBrowser();
     })
 
-    
     const performSearch = async () => {
         const page = await browserWrapper.getPage(websiteUrl);
         const searchPage = new SearchPage(page);
@@ -45,12 +41,6 @@ test.describe('search test', ()=>{
         expect(await searchPage.isSortedLowToHigh()).toBeTruthy();
         
     });
-
-    //test('Perform Total Search', async () => {
-      //  const searchPage = await performSearch();
-        //expect(searchPage.productListCount).toBe(searchPage.expectedCount);
-        
-    //});
  })
 
 
