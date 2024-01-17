cb765cd493c7f998198a041f53dd1cb8b12e6c68
waaed azzam <waaed013@gmail.com>
2024-01-17 18:42:41 +0200

    fix some bugs

--- logic/ui/cart-page.ts
+++ logic/ui/cart-page.ts
@@ -4,18 +4,18 @@ import { BasePage } from "../../infra/ui/base-page";
 export class CartPage extends BasePage {
 
     private itemName = (itemName: string) => this.page.locator(`//a[@class="tx-link-a name_1GBQ tx-link_29YD" and text()="${itemName}"]`)
-    private freeShippingTitle:Locator
+    private freeShippingTitle: Locator
     constructor(page: Page) {
         super(page);
         this.freeShippingTitle = page.locator('//div[@class="title_1Fq3"]')
         this.initPage();
     }
 
-    getItemTitleLocator =  (itemName: string) => {
+    getItemTitleLocator = (itemName: string) => {
         return this.itemName(itemName)
     };
 
-    getFreeShippingTitle =async ():Promise<string> => {
+    getFreeShippingTitle = async (): Promise<string> => {
         return await this.freeShippingTitle.innerText()
     }
 }
